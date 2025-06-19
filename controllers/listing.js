e0855d8caa35929ce973709a96a6a2feb8f0b729
ADITYA BHAN <147151572+adityabhan007@users.noreply.github.com>
2025-06-19 12:48:47 +0530

    Update listing.js

--- controllers/listing.js
+++ controllers/listing.js
@@ -24,12 +24,12 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-  let url = req.file.path;
-  let filename = req.file.filename;
+  // let url = req.file.path;
+  // let filename = req.file.filename;
 
   let newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
-  newListing.image = { url, filename };
+  // newListing.image = { url, filename };
   await newListing.save();
   req.flash("success", "Successfully created a new listing");
   res.redirect("/listings");
