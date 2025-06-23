0e962f95730763066bbcaa5d1dc326089a6aba1e
ADITYA BHAN <147151572+adityabhan007@users.noreply.github.com>
2025-06-23 08:40:43 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -12,6 +12,8 @@ const upload = multer({ storage });
 
 router
   .route("/")
+  // create home route
+  .get(wrapasync(listingController.home))
   // create index route
   .get(wrapasync(listingController.index))
   //  create post route to new listing
